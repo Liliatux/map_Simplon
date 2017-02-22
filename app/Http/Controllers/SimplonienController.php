e1e6d4cb6d8067f25653383480e4875aa21153f3
Lilia <geertszelia@gmail.com>
2017-02-22 16:25:08 +0100

    Ajout des données du json dans une base de donnée

--- app/Http/Controllers/SimplonienController.php
+++ app/Http/Controllers/SimplonienController.php
@@ -2,18 +2,39 @@
 
 namespace App\Http\Controllers;
 
-use App\Simploniens;
+use App\Simplonien;
 use Illuminate\Http\Request;
 
 class SimplonienController extends Controller
 {
     public function getData () {
+    	//récupère les données du fichier json
     	$jsonData = file_get_contents('../public/simploniensGeo.geojson');
     	$data = json_decode($jsonData, true);
+    	$datas = $data['features'];
 
-    	return $data['id'];
-    	// $simplonien = new Simplonien;
-    	// $simplonien->id = $data['']
+    	//intègre les données du json dans mySql
+    	foreach ($datas as $value){
+    		$simplonien = new Simplonien;
+    		
+    		$simplonien->nom = $value['properties']['nom'];
+    		$simplonien->prenom = $value['properties']['prenom'];
+    		$simplonien->codePostale = $value['properties']['codePostal'];
+    		$simplonien->cv = $value['properties']['cv'];
+    		$simplonien->punchline = $value['properties']['punchline'];
+    		$simplonien->site = $value['properties']['sitePerso'];
+    		$simplonien->github = $value['properties']['github'];
+    		$simplonien->twitter = $value['properties']['twitter'];
+    		$simplonien->linkedin = $value['properties']['linkedin'];
+    		$simplonien->facebook = $value['properties']['facebook'];
+    		$simplonien->longitude = $value['geometry']['coordinates'][0];
+    		$simplonien->lattitude = $value['geometry']['coordinates'][1];
+    		
+    		$simplonien->save();
+    	}
+
+
+    	return view('map');
     }
 
 }
